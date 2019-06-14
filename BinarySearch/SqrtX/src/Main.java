07c5e4432dae1f7029b53c4f1a3d0d5f67a37259
SinLapis <stradust0001@gmail.com>
2019-06-14 16:48:11 +0800

    0614

--- BinarySearch/SqrtX/src/Main.java
+++ BinarySearch/SqrtX/src/Main.java
@@ -4,27 +4,29 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Random r = new Random();
-        int[] param = new int[1000000];
-        for (int i = 0; i < 1000000; i++) {
-            param[i] = r.nextInt();
-        }
-        Solution2 s2 = new Solution2();
-        long startTime = System.currentTimeMillis();   //获取开始时间
-        for (int p : param) {
-            Math.sqrt((double)p);
-        }
-        System.out.println();
-        long endTime = System.currentTimeMillis(); //获取结束时间
-        System.out.println("程序运行时间： " + (endTime - startTime) + "ms");
+//        Random r = new Random();
+//        int[] param = new int[1000000];
+//        for (int i = 0; i < 1000000; i++) {
+//            param[i] = r.nextInt();
+//        }
+//        Solution2 s2 = new Solution2();
+//        long startTime = System.currentTimeMillis();   //获取开始时间
+//        for (int p : param) {
+//            Math.sqrt((double)p);
+//        }
+//        System.out.println();
+//        long endTime = System.currentTimeMillis(); //获取结束时间
+//        System.out.println("程序运行时间： " + (endTime - startTime) + "ms");
+//        Solution s = new Solution();
+//        startTime = System.currentTimeMillis();   //获取开始时间
+//        for (int p : param) {
+//            s.mySqrt(p);
+//        }
+//        System.out.println();
+//        endTime = System.currentTimeMillis(); //获取结束时间
+//        System.out.println("程序运行时间： " + (endTime - startTime) + "ms");
         Solution s = new Solution();
-        startTime = System.currentTimeMillis();   //获取开始时间
-        for (int p : param) {
-            s.mySqrt(p);
-        }
-        System.out.println();
-        endTime = System.currentTimeMillis(); //获取结束时间
-        System.out.println("程序运行时间： " + (endTime - startTime) + "ms");
+        System.out.println(s.mySqrt(99));
     }
 }
 
@@ -35,7 +37,7 @@ class Solution {
             a <<= 1;
             r = (r << 2) + (x >> 30);
             x <<= 2;
-            t = a << 1 + 1;
+            t = (a << 1) + 1;
             if (t <= r) {
                 r -= t;
                 a++;
